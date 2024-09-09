0b7c7af28801b615ccab7841ce72132acc416693
Yash Chauhan <forcourse1234@gmail.com>
2024-09-09 11:40:22 +0530

    added product controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,24 +1,12 @@
 import express from "express";
+import { addProduct, getProductViaId } from "../controllers/productController.js";
 
 const router = express.Router();
 
 // Get Product via id
-router.get("/:id", (req, res) => {
-  const productId = req?.params?.id;
-  if (!productId) {
-    return res.status(404).send("Product id is required");
-  }
-  res.send(`Product id is ${productId}`);
-});
+router.get("/:id", getProductViaId);
 
 // Add Product
-router.post("/", (req, res) => {
-  const product = req?.body;
-  if (!product) {
-    return res.status(404).send("Product details are required");
-  }
-
-  res.send(`Product details are ${JSON.stringify(product)}`);
-});
+router.post("/", addProduct);
 
 export default router;
